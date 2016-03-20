65252e22a2f2e2d32fa16b2cd7c7bf649d4392ce
sebastiao <sebastiao.miranda@outlook.com>
2016-03-20 22:37:43 +0000

    Added header guards; re-formated comments

--- microunit.h
+++ microunit.h
@@ -1,25 +1,25 @@
 /**
-* This file is part of the MicroUnit c++ unit testing library.
+* @file microunit.h
+* @author Sebastiao Salvador de Miranda (ssm)
+* @copyright Copyright (c) 2016, Sebastiao Salvador de Miranda.
+*            All rights reserved.
 *
-* Copyright (c) 2016, Sebastiao Miranda
-* All rights reserved.
-* 
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
 * met:
-* 
+*
 * (1) Redistributions of source code must retain the above copyright
 * notice, this list of conditions and the following disclaimer.
-* 
+*
 * (2) Redistributions in binary form must reproduce the above copyright
 * notice, this list of conditions and the following disclaimer in
 * the documentation and/or other materials provided with the
 * distribution.
-* 
+*
 * (3) The name of the author may not be used to
 * endorse or promote products derived from this software without
 * specific prior written permission.
-* 
+*
 * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -31,11 +31,6 @@
 * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 * POSSIBILITY OF SUCH DAMAGE.
-*/
-/**
-* @file microunit.h
-* @author Sebastiao Miranda
-* @copyright 
 *
 * @brief Tiny library for cpp unit testing. Should work on any c++11 compiler.
 *
@@ -61,6 +56,8 @@
 * @endcode
 */
 
+#ifndef _MICROUNIT_MICROUNIT_H_
+#define _MICROUNIT_MICROUNIT_H_
 #include <map>
 #include <string>
 #include <vector>
@@ -230,3 +227,4 @@ FAIL();                                                                        \
 std::cout << "[    ] Test Assert failed: " #condition << std::endl;            \
 FAIL();                                                                        \
 }
+#endif
